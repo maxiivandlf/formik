b42efc4e32219593f994e8eee89036383411f05f
MaxiIvan <85696496+maxiivandlf@users.noreply.github.com>
2022-04-16 19:06:12 -0300

    update components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { Formik, Form } from 'formik';
+import { Formik, Form, ErrorMessage } from 'formik';
 import TextInput from './components/TextInput';
 import Checkbox from './components/Checkbox';
+import Radio from './components/Radio';
 import Select from './components/Select';
 
 const validate = (values) => {
@@ -15,13 +16,23 @@ const validate = (values) => {
   } else if (values.lastname.length < 5) {
     errors.lastname = 'El apellido es my corto';
   }
+
+  if (!values.radio) {
+    errors.radio = 'Requerido';
+  }
   return errors;
 };
 
 function App() {
   return (
     <Formik
-      initialValues={{ name: '', lastname: '', email: '', profesion: '' }}
+      initialValues={{
+        name: '',
+        lastname: '',
+        email: '',
+        profesion: '',
+        radio: '',
+      }}
       validate={validate}
       onSubmit={(values) => console.log(values)}>
       <Form>
@@ -39,6 +50,14 @@ function App() {
         </Select>
 
         <Checkbox name='accept'>Aceptar terminos y condiciones</Checkbox>
+
+        <Radio name='radio' value='chancho1' label='Chancho 1' />
+        <Radio name='radio' value='chancho2' label='Chancho 2' />
+        <Radio name='radio' value='chancho3' label='Chancho 3' />
+
+        <ErrorMessage name='radio' />
+        <br />
+
         <button type='submit'>Enviar</button>
       </Form>
     </Formik>
